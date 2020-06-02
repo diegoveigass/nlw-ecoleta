7a7d557922f09bbe9552f94feb07430b556bf923
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-06-02 10:01:17 -0300

    eslint config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,14 +3,18 @@ import express from 'express';
 const app = express();
 
 app.get('/users', (request, response) => {
-  console.log('Listagem de usuários')
+  console.log('Listagem de usuários');
 
-  response.json([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Diegao'
-  ])
-})
+  response.json(['Diego', 'Cleiton', 'Robson', 'Diegao']);
+});
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Diego',
+    email: 'diegoveiga@gmail.com',
+  };
+
+  return response.json(user);
+});
 
 app.listen(3333);

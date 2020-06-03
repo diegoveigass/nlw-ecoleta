fc0d09d0b9228af8662e4f77402b81aba50431f2
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-06-03 11:05:07 -0300

    config eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Next Level Week</h1>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <h1>Olá</h1>;
+};
 
 export default App;
